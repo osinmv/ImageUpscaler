9499c5cddb1297c3aa18bff609659497044fcd5e
osinmv <31288523+osinmv@users.noreply.github.com>
2023-04-01 14:33:26 -0400

    Added a bit more channels, getting slightly better image.

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from dataset import UpscalerDataset
 mps_device = torch.device("mps")
 model = torch.nn.Sequential(
     torch.nn.UpsamplingBilinear2d(size=(1200, 1600)),
-    torch.nn.Conv2d(1, 16, kernel_size=9, padding=4),
+    torch.nn.Conv2d(1, 32, kernel_size=9, padding=4),
     torch.nn.ReLU(),
-    torch.nn.Conv2d(16, 8, kernel_size=1, padding=0),
+    torch.nn.Conv2d(32, 16, kernel_size=1, padding=0),
     torch.nn.ReLU(),
-    torch.nn.Conv2d(8, 1, kernel_size=5, padding=2),
+    torch.nn.Conv2d(16, 1, kernel_size=5, padding=2),
 )
 
 model.to(mps_device)
